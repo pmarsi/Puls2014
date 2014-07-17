1741dc70cbdda8a2106e8cb34be2b0180fa1b701
pmarsi <pedromartinsilva@MacBook-Pro-de-pedro.local>
2014-07-17 18:10:42 +0200

    Terminado formulario con jquery

--- formulario.js
+++ formulario.js
@@ -1,33 +1,46 @@
 $(function(){
+	//mostrar el formulario
+
+	//esta variable es igual al selector que se referencia mediante el id
 	var $form = $('#formulario');
 	var $titulo = $('#titulo');
-	var $url = $('#Link');
-	var $primerPost = $('.item').first();
+	var $url = $('#link');
+	var $primerPost = $('.item').first(); //hace referencia al primer item, clona el primer item
+	//modificara una variable, la variable form
 	var $lista = $('#contenido');
-
-	function mostrarOcultarFormulario(){
+	function mostrarOcultarFormulario()
+	{
+		//mira si esta visible, si no lo muestra y si esta visible lo oculta.
 		$form.slideToggle();
-		return false;
+		return false; //para evitar los saltos en los links
+		//también se encarga de que el evento no prosiga.
 	}
 
-	function agregarPost(){
-		var titulo = $titulo.val();//obtengo el valor de ese elemento
+	//interaccion con el fomulario
+
+	function agregarPost()
+	{
+		//coge la informacion de los inputs
+		var titulo = $titulo.val();
 		var url = $url.val();
+
+		//ahora vamos a clonar, clono un article para luego ir añadiendo
+		//genera una replica de lo que clonamos. luego asignaremos propiedades
 		var clone = $primerPost.clone();
 
 		clone.find('.titulo_item a')
 			.text(titulo)
-			.attr('href', url)
-			
+			.attr('href', url) //para referencia href se hace mediante el atributo y le asignamos el valor con la variable url
+		
 		clone.hide()
 
 		$lista.prepend(clone)
+
 		clone.slideDown()
 
-
-
 	}
-	$('#publicar_nav a').click( mostrarOcultarFormulario )
+	$('#publicar_nav a').click(mostrarOcultarFormulario)
+	//tenemos que escuchar el evento de submit, no se activa si el formualario no es valido
 	$('#formulario').on('submit', agregarPost)
 
 });
